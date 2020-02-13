40cbca7714e3ef0498d2240922d6830553a31a30
Sandra Coburn <sandra_coburn@hotmail.com>
2020-02-13 12:55:13 -0800

    Added script start to deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// code away!
 const server = require("./server.js");
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n** Server Running on Http://localhost:${port} **\n`);
 });
